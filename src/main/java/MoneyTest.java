d40e0cb27a7aaca0da8575f9273df145a439b3b4
GorskiJakub <jakubg93@o2.pl>
2015-04-23 19:38:30 +0200

    Dodanie pierwszego testu dla metody isEquals

--- src/main/java/MoneyTest.java
+++ src/main/java/MoneyTest.java
@@ -1,13 +1,16 @@
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+ 
+ public class MoneyTest {
+ 
+ 	@Test
+	public void test_ifBothAreEquals() {
+		Money m1 = new Money(5.00, "USD");
+		Money m2 = new Money(5.00, "USD");
 
-
-public class MoneyTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+		assertThat(m1, is(equalTo(m2)));
+ 	}
+ 
+ }
